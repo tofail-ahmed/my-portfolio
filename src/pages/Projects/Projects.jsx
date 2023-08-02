09cc8152668b275ebf6b57be06cd8f4af671aee6
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-08-02 20:07:21 +0600

    new-project:campus-gurur added

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import kiddo from '../../assets/kiddo-valley-cartoon.png'
 import excel from '../../assets/athletic-cartoon.png'
 import cuisine from '../../assets/quisinne-cartoon.png'
+import college from '../../assets/admission-guru.png'
 import portfolio from '../../assets/portfolio-cartoon.png'
 import Aos from 'aos';
 import './Projects.css'
@@ -17,6 +18,32 @@ const Projects = () => {
             <div id='projects' className='my-44'>
                   <h1 className=' glow-text section-name text-center my-8'>My developments</h1>
                   <div className='grid md:grid-cols-2  gap-8 my-12 '>
+                        <div className='bgcontainer'>
+                              <div className=" boat card md:w-[500px]  text-slate-300 shadow-xl mx-auto bg-green-800" >
+                                    <div>
+                                          <a href="https://college-client-adeee.web.app/" target='_blank'> <figure><img className='md:w-[550px] md:h-[250px]' src={college} alt="Shoes" /></figure></a>
+                                    </div>
+                                    <div className='text-emerald-950 text text-start m-4 font-bold'>Campus Guru - an addmission website where ampuses with details are found</div>
+                                    <div className='m-4 grid grid-cols-4'>
+                                          <span data-aos='fade-up-left' data-aos-delay='700' data-aos-duration='700' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>HTML</span>
+                                          <span data-aos='fade-up-right' data-aos-delay='800' data-aos-duration='800' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>CSS</span>
+                                          <span data-aos='fade-down-left' data-aos-delay='900' data-aos-duration='900' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>JS</span>
+                                          <span data-aos='fade-down-right' data-aos-delay='1000' data-aos-duration='1000' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>ResctJS</span>
+                                          <span data-aos='flip-left' data-aos-delay='1100' data-aos-duration='1100' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>Tailwind</span>
+                                          <span data-aos='flip-right' data-aos-delay='1200' data-aos-duration='1200' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>DaisyUI</span>
+                                          <span data-aos='fade-down-left' data-aos-delay='900' data-aos-duration='1300' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>Firebase</span>
+                                          <span data-aos='fade-down-right' data-aos-delay='1000' data-aos-duration='1400' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>NodeJS</span>
+                                          <span data-aos='flip-right' data-aos-delay='1200' data-aos-duration='   1500' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>ExpressJS</span>
+                                          <span data-aos='fade-down-left' data-aos-delay='900' data-aos-duration='1600' className='text-md text-green-900 bg-green-400 rounded-md font-bold w-[80px] text-center m-2 '>MongoDB</span>
+                                    </div>
+                                    <div className='my-4 mx-auto'>
+                                          <a href="https://github.com/tofail-ahmed/college-client" target='_blank'><span className='text-md text-lime-900 bg-lime-400 rounded-sm font-bold w-[80px] text-center m-2 '>Github</span></a>
+
+                                          <a href="https://college-client-adeee.web.app/" target='_blank'><span className='text-md text-lime-900 bg-lime-400 rounded-sm font-bold w-[80px] text-center m-2 '>Live Site</span></a>
+
+                                    </div>
+                              </div>
+                        </div>
 
                         <div className='bgcontainer'>
                               <div className=" boat card md:w-[500px]  text-slate-300 shadow-xl mx-auto bg-green-800" >
@@ -97,7 +124,7 @@ const Projects = () => {
                         <div className='bgcontainer'>
                               <div className=" boat card md:w-[500px]  text-slate-300 shadow-xl mx-auto bg-green-800" >
                                     <div>
-                                          <a href="https://tofailahmedportfolio.netlify.app/" target='_blank'> <figure><img src={portfolio} alt="Shoes" /></figure></a>
+                                          <a href="https://tofailahmedportfolio.netlify.app/" target='_blank'> <figure><img className='md:h-[250px] md:w-[550px]' src={portfolio} alt="Shoes" /></figure></a>
                                     </div>
                                     <div className='text-emerald-950 text text-start m-4 font-bold'>My Developer Porfolio</div>
                                     <div className='m-4 grid grid-cols-4'>
